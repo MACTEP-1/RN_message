bba487cf82870748b1656b4cdfd73c2dfa9b75da
Larion Tyshler <oracle_11@hotmail.com>
2024-08-25 20:25:21 -0700

    Added photo pick

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, TextInput, Button, Image, Text } from "react-native";
-
+import Styles from "./Styles";
+import ImageChooser from "./ImagePick";
 const UserInfo = () => {
+    const[name, setName] = useState("");
+    const[image, setImage] = useState("");
     return (
-        <View>
-            <Image source={require("../assets/logo2.jpg")}/>
+        <View style={Styles.personalInfoContainer}>
+            <Image 
+                style={Styles.logo}
+                source={require("../assets/logo2.jpg")}
+            ></Image>
 
-            <View>
-                <Text>Enter YOUR name:</Text>
-                <TextInput />
+            <View style={Styles.enterYourName}>
+                <Text style={Styles.nameText}>Enter name:</Text>
+                <TextInput style={Styles.nameTextInput} 
+                    onChangeText={(text) => setName(text)}
+                    value = {name}
+                />
             </View>
+
+            <ImageChooser onChangeImage={(image) => setImage(image)} />
 
             <Button title="Start" onPress={() => {}} />
         </View>
